000c0c260fc23890b69d9c30539497eba89c1468
ShadeSWD <seawolfy60@gmail.com>
2024-02-12 20:47:03 +0300

    tests

--- files/urls.py
+++ files/urls.py
@@ -6,7 +6,7 @@ from .views import FileCreate, FileList, HomeView
 app_name = FilesConfig.name
 
 urlpatterns = [
-    path("", HomeView.as_view()),
-    path("upload/", FileCreate.as_view()),
-    path("files/", FileList.as_view()),
+    path("", HomeView.as_view(), name="home"),
+    path("upload/", FileCreate.as_view(), name="file_create"),
+    path("files/", FileList.as_view(), name="file_list"),
 ]
